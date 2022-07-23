c7b4ee0c680cf8d529700782df8628766ca2d8ab
Mateus Pragana <mateuspragana@gmail.com>
2022-07-23 19:04:26 -0300

    Finalizando aula "CSS Modules"

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
+
+import "./styles.css"
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Diego Fernandes"
         content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit deserunt nostrum numquam quisquam libero tempore, ipsum incidunt non repellendus, iste facilis? Recusandae accusantium tenetur asperiores accusamus delectus impedit unde odio?"
