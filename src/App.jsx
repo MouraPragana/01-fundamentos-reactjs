ba59f279a99d83f1865b9817612a7e8952d9d221
Mateus Pragana <mateuspragana@gmail.com>
2022-07-23 18:43:59 -0300

    Finalizando aula "Propriedades"

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { Post } from "./Post"
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Diego Fernandes"
+        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit deserunt nostrum numquam quisquam libero tempore, ipsum incidunt non repellendus, iste facilis? Recusandae accusantium tenetur asperiores accusamus delectus impedit unde odio?"
+      />
+      <Post
+        author="Gabriel Buzzi"
+        content="Um novo post muito legal"
+      />
     </div>
   )
 }
