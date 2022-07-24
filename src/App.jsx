7c666517bc278b989140887d9de544c46ccc1948
Mateus Pragana <mateuspragana@gmail.com>
2022-07-23 21:48:25 -0300

    Primeira aula "Componente: Sidebar"

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,26 @@ import { Header } from "./components/Header"
 import { Post } from "./Post"
 
 import "./global.css"
+import styles from "./App.module.css"
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Diego Fernandes"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit deserunt nostrum numquam quisquam libero tempore, ipsum incidunt non repellendus, iste facilis? Recusandae accusantium tenetur asperiores accusamus delectus impedit unde odio?"
-      />
-      <Post
-        author="Gabriel Buzzi"
-        content="Um novo post muito legal"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit deserunt nostrum numquam quisquam libero tempore, ipsum incidunt non repellendus, iste facilis? Recusandae accusantium tenetur asperiores accusamus delectus impedit unde odio?"
+          />
+          <Post
+            author="Gabriel Buzzi"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
     </div>
   )
 }
