83f8b0cbc07cb99d30fc68bb133e8cc3cfcc52c6
Mateus Pragana <mateuspragana@gmail.com>
2022-07-27 04:44:32 -0300

    Finalizando aula "Inserindo comentários (Programação declarativa)"

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,7 +9,8 @@ import styles from "./Post.module.css"
 
 export function Post({ author, publishedAt, content }) {
 
-    const [comments, setComments] = useState([1, 2])
+    const [comments, setComments] = useState([])
+    const [newCommentText, setNewCommentText] = useState("")
 
     const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'as' HH:mm'h'", {
         locale: ptBR
@@ -21,8 +22,13 @@ export function Post({ author, publishedAt, content }) {
     })
 
     function handleCreateNewComment() {
-        event.preventDefault()
-        setComments([...comments, comments.length + 1])
+        event.preventDefault();
+        setComments([...comments, newCommentText]);
+        setNewCommentText("");
+    }
+
+    function handleNewCommentChange() {
+        setNewCommentText(event.target.value);
     }
 
     return (
@@ -54,7 +60,10 @@ export function Post({ author, publishedAt, content }) {
             <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
                 <textarea
+                    name="comment"
                     placeholder="Deixe um comentário"
+                    onChange={handleNewCommentChange}
+                    value={newCommentText}
                 />
                 <footer>
                     <button type="submit">Publicar</button>
@@ -63,7 +72,7 @@ export function Post({ author, publishedAt, content }) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment />
+                    return <Comment content={comment} />
                 })}
             </div>
 
