e355c4df38f3c2b7cfbfcedf2d4546040a74d4f7
Mateus Pragana <mateuspragana@gmail.com>
2022-07-30 22:29:45 -0300

    Finalizando aula "Validando formulário"

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -28,17 +28,24 @@ export function Post({ author, publishedAt, content }) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity("");
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity("Este campo é obrigatório !");
     }
 
     function deleteComment(commentToDelete) {
 
         const commentsWithoutDeletedOne = comments.filter(comment => {
             return comment !== commentToDelete;
-        })
+        });
 
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
 
     return (
         <article className={styles.post}>
@@ -73,9 +80,11 @@ export function Post({ author, publishedAt, content }) {
                     placeholder="Deixe um comentário"
                     onChange={handleNewCommentChange}
                     value={newCommentText}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>
                 </footer>
             </form>
 
